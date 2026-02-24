afa81dd35397621758e9e2d37a908aaf4e9774e3
IamVarunnn <varunrakesh16@gmail.com>
2026-02-24 15:47:41 +0530

    Done

---
+++ JavaScript/Practice Level 4/script.js
@@ -0,0 +1,16 @@
+
+const name = "Kg Coding";
+
+let heading = document.querySelector("h1");
+
+heading.innerText = name;
+
+const totalCost = 2 * 4 * 5 / 2 * 9 + 1;
+console.log(totalCost);
+
+const gstPer = 18/100;
+
+const val = '45';
+const val2 = '44';
+
+console.log(eval('45 + 50'));
