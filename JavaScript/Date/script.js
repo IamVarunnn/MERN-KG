70c3d68be93317f9c3d293ca61b0a82cda97ccbe
IamVarunnn <varunrakesh16@gmail.com>
2026-03-07 14:53:32 +0530

    Done

---
+++ JavaScript/Date/script.js
@@ -0,0 +1,10 @@
+
+
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.getDay());
+console.log(myDate.getFullYear());
+console.log(myDate.getHours());
+console.log(myDate.getMinutes());
+console.log(myDate.getSeconds());
+console.log(myDate.getMilliseconds())
