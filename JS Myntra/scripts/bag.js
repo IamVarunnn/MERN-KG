336337503c8e88aea4139e5f130089339101cec7
IamVarunnn <varunrakesh16@gmail.com>
2026-03-24 18:36:01 +0530

    Finished Myntra Follow Up Project

---
+++ JS Myntra/scripts/bag.js
@@ -0,0 +1,159 @@
+{/*
+    <div class="bag-items-container">
+    <div class="bag-item-container">
+    <div class="item-left-part">
+        <img class="bag-item-img" src="../images/4.jpg">
+    </div>
+    <div class="item-right-part">
+        <div class="company">ADIDAS</div>
+        <div class="item-name">Men Printed Polo Collar Indian Cricket ODI Jersey</div>
+        <div class="price-container">
+        <span class="current-price">Rs 999</span>
+        <span class="original-price">Rs 999</span>
+        <span class="discount-percentage">(0% OFF)</span>
+        </div>
+        <div class="return-period">
+        <span class="return-period-days">14 days</span> return available
+        </div>
+        <div class="delivery-details">
+        Delivery by
+        <span class="delivery-details-days">10 Oct 2023</span>
+        </div>
+    </div>
+
+    <div class="remove-from-cart">X</div>
+    </div>
+
+</div> 
+ */}
+
+
+
+let bagItemObjects;
+const convenienceFee = 99;
+onLoad();
+function onLoad(){
+    loadBagItemObjects();
+    displayBagItems();
+    displayBagSummary();
+
+}
+
+function displayBagSummary(){
+
+    let bag_summary = document.querySelector(".bag-summary");
+    let totalItem = bagItemObjects.length;
+    let totalMRP = 0;
+    let totalDiscount = 0;
+
+    let finalPayment = 0;
+
+
+    bagItemObjects.forEach(bagItem =>{
+        totalMRP += bagItem.original_price;
+        totalDiscount += bagItem.original_price - bagItem.current_price;
+    });
+
+    finalPayment = (totalMRP - totalDiscount) + convenienceFee;
+
+    bag_summary.innerHTML = `
+        <div class="bag-details-container">
+            <div class="price-header">PRICE DETAILS (${totalItem} Items) </div>
+            <div class="price-item">
+              <span class="price-item-tag">Total MRP</span>
+              <span class="price-item-value">Rs ${totalMRP}</span>
+            </div>
+            <div class="price-item">
+              <span class="price-item-tag">Discount on MRP</span>
+              <span class="price-item-value priceDetail-base-discount">-Rs ${totalDiscount} </span>
+            </div>
+            <div class="price-item">
+              <span class="price-item-tag">Convenience Fee</span>
+              <span class="price-item-value">Rs ${convenienceFee}</span>
+            </div>
+            <hr>
+            <div class="price-footer">
+              <span class="price-item-tag">Total Amount</span>
+              <span class="price-item-value">Rs ${finalPayment}</span>
+            </div>
+          </div>
+          <button class="btn-place-order">
+            <div class="css-xjhrni">PLACE ORDER</div>
+          </button>
+    `
+
+}
+
+function loadBagItemObjects(){
+    console.log(bagItems);
+
+    bagItemObjects = bagItems.map(itemId => {
+
+        for(let i = 0; i < items.length; i++){
+
+            if(itemId == items[i].id){
+                return items[i];
+            }
+        }
+
+    });
+
+    console.log(bagItemObjects);
+}
+
+
+function displayBagItems(){
+
+    // console.log(bagItems);
+
+    let container_items = document.querySelector(".bag-items-container");
+
+    let innerHTML = '';
+
+    bagItemObjects.forEach(element => {
+        innerHTML += generateItemHtml(element);
+    });
+
+    container_items.innerHTML = innerHTML;
+}
+
+function removeFromBag(itemId){
+
+     
+    bagItems = bagItems.filter(bagItemId => bagItemId != itemId);
+    localStorage.setItem('bag-items',JSON.stringify(bagItems));
+    loadBagItemObjects();
+    displayBagIcon();
+    displayBagItems();
+    displayBagSummary();
+}
+
+function generateItemHtml(item){
+
+    return `
+     
+        <div class="bag-item-container">
+            <div class="item-left-part">
+              <img class="bag-item-img" src="../${item.image}">
+            </div>
+            <div class="item-right-part">
+              <div class="company">${item.company}</div>
+              <div class="item-name">${item.item_name}</div>
+              <div class="price-container">
+                <span class="current-price">Rs${item.current_price}</span>
+                <span class="original-price">Rs ${item.original_price}</span>
+                <span class="discount-percentage">(${item.discount_percentage}% OFF)</span>
+              </div>
+              <div class="return-period">
+                <span class="return-period-days">${item.return_period} days</span> return available
+              </div>
+              <div class="delivery-details">
+                Delivery by
+                <span class="delivery-details-days">${item.delivery_date}</span>
+              </div>
+            </div>
+
+            <div class="remove-from-cart" onclick = "removeFromBag(${item.id});" >X</div>
+        </div>
+    `;
+}
--- JS Myntra/scripts/index.js
+++ JS Myntra/scripts/index.js
@@ -29,33 +29,76 @@
 //     dicount_percentage : 42
 // };
 
-let items_container = document.querySelector(".items-container");
 
-let innerHTML = '';
+let bagItems;
+onLoad();
 
+function onLoad(){
 
+    let bagItemsStr = localStorage.getItem('bag-items');
+
+    bagItems = bagItemsStr ? JSON.parse(bagItemsStr) : [];
+
+    displayItemsOnHomePage();
+    displayBagIcon();
+}
+
+function addToBag(itemId){
+    
+    bagItems.push(itemId);
+
+    localStorage.setItem('bag-items', JSON.stringify(bagItems));
+    // console.log(itemId);
+    // console.log(bagItems);
+    displayBagIcon();
+}
+
+function displayBagIcon(){
+    let bag_item_count = document.querySelector(".bag-item-count");
+
+    if(bagItems.length > 0){
+        bag_item_count.style.visibility = 'visible';
+        bag_item_count.innerText = bagItems.length;
+    }
+    else{
+        bag_item_count.style.visibility = 'hidden';
+    }
+
+    
+}
+
+function displayItemsOnHomePage(){
+    let items_container = document.querySelector(".items-container");
+    let innerHTML = '';
+
+    if(!items_container){
+        return;
+    }
  
-items.forEach(item =>{
-    innerHTML += `
-        <div class="item-container">
-            <img class="item-image" src="${item.image} " alt="">
-            <div class="rating">
-                <span>${item.rating.stars} ⭐ | ${item.rating.count}</span>
+    items.forEach(item =>{
+        innerHTML += `
+            <div class="item-container">
+                <img class="item-image" src="${item.image} " alt="">
+                <div class="rating">
+                    <span>${item.rating.stars} ⭐ | ${item.rating.count}</span>
+                </div>
+                <div class="company-name"> ${item.company} </div>
+                <div class="item-name">${item.item_name} </div>
+                <div class="price">
+                    <span class="current-price" >Rs${item.current_price} </span>
+                    <span class="original-price" >Rs${item.original_price}</span>
+                    <span class="discount-percentage" >(${item.discount_percentage}% OFF)</span>
+                </div>
+                <button class="btn-add-bag" onclick = "addToBag(${item.id});" >Add to Bag</button>
             </div>
-            <div class="company-name"> ${item.company} </div>
-            <div class="item-name">${item.item_name} </div>
-            <div class="price">
-                <span class="current-price" >Rs${item.current_price} </span>
-                <span class="original-price" >Rs${item.original_price}</span>
-                <span class="discount-percentage" >(${item.discount_percentage}% OFF)</span>
-            </div>
-            <button class="btn-add-bag" >Add to Bag</button>
-        </div>
-    `
-});
+        `
+    });
+
+    items_container.innerHTML = innerHTML;
+}
 
 
 
-items_container.innerHTML = innerHTML;
+
 
 
